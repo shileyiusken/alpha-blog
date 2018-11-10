2890c93838765275dc0c178fdc0fc5396fcc393d
shileyiusken <shileyiusken@gmail.com>
2018-11-10 11:23:42 +0800

    editing session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+
+    end
+
+    def logged_in?
+
+    end
+
+    def require_user
+
+    end
+
 end
